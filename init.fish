319e015dd0f99b213b34a73c79b0f61fa2c89d8a
Linh M. Tran <linhtm@vietnamlab.vn>
2020-06-03 10:04:54 +0700

    add comment to drop cache

--- init.fish
+++ init.fish
@@ -9,7 +9,7 @@ set -xg JAVA_HOME "/home/linuxbrew/.linuxbrew/opt/openjdk"
 set -xg VISUAL "vim"
 set -xg EDITOR "$VISUAL"
 # Set path
-set --universal fish_user_paths $HOME/bin $HOME/.local/bin $HOME/go/bin $GOPATH/bin $HOME/.cargo/bin $HOME/.krew/bin $HOME/.composer/vendor/bin/ /home/linuxbrew/.linuxbrew/opt/openjdk/bin 
+set --universal fish_user_paths $HOME/bin $HOME/.local/bin $HOME/go/bin $GOPATH/bin $HOME/.cargo/bin $HOME/.krew/bin $HOME/.composer/vendor/bin /home/linuxbrew/.linuxbrew/opt/openjdk/bin 
 
 # Base16 Shell
 if status --is-interactive
@@ -19,6 +19,7 @@ end
 
 # Linuxbrew
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 # Ssh agent
 set -xg SSH_AUTH_SOCK $HOME/.ssh/agent.sock
 ss -a | grep -q $SSH_AUTH_SOCK
@@ -26,10 +27,13 @@ if test $status -ne 0
     rm -f $SSH_AUTH_SOCK
     setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/c/tools/wsl-ssh-agent/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
 end
+
 # dir colors
 bass (dircolors ~/wsl_dotfiles/dircolors.base16.dark)
+
 # Directory
 alias dev='cd $HOME/Dev'
+
 # tmux
 alias tma='tmux attach -t'
 alias tmd='tmux new -s (basename (pwd))'
@@ -37,4 +41,8 @@ alias tmn='tmux new -s'
 alias tml='tmux list-sessions'
 alias tmk='tmux kill-session -t'
 
-
+# drop cache
+# This is specific to WSL 2. If the WSL 2 VM goes rogue and decides not to free
+# up memory, this command will free your memory after about 20-30 seconds.
+# Details: https://github.com/microsoft/WSL/issues/4166#issuecomment-628493643
+alias drop_cache="sudo sh -c \"echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a && printf '\n%s\n' 'Ram-cache and Swap Cleared'\"" 
